be3d891eb7dc9adaed6f625a16a9c78d339c64a9
cdb96 <kiilk1@163.com>
2025-05-04 11:56:06 +0800

    使用NEON进行向量化优化，进一步减少RC4解密时延

--- app/src/main/cpp/RC4Decrypt.cpp
+++ app/src/main/cpp/RC4Decrypt.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <arm_neon.h>
 
-uint8_t keyStreamBytes[256];
+uint8_t keyStreamBytes[257];
 extern "C"
 JNIEXPORT jbyteArray JNICALL
 Java_com_cdb96_ncmconverter4a_RC4Jni_ksa(JNIEnv* env, jclass, jbyteArray key) {
@@ -24,6 +24,8 @@ Java_com_cdb96_ncmconverter4a_RC4Jni_ksa(JNIEnv* env, jclass, jbyteArray key) {
     for (int k = 0; k < 256; ++k) {
         keyStreamBytes[k] = sBox[ (sBox[k] + sBox [ ( sBox[k] + k ) & 0xff ] ) & 0xff ];
     }
+    //下面的代码进行到i = 256 * n - 15的时候会出现数组越界，迫不得已只能这么搞了
+    keyStreamBytes[256] = sBox[ (sBox[0] + sBox [ ( sBox[0] + 0 ) & 0xff ] ) & 0xff ];
 
     jbyteArray result = env->NewByteArray(256);
     env->SetByteArrayRegion(result, 0, 256, reinterpret_cast<const jbyte*>(sBox));
@@ -38,7 +40,15 @@ Java_com_cdb96_ncmconverter4a_RC4Jni_prgaDecrypt(JNIEnv* env, jclass, jbyteArray
     auto* sBoxBytes = reinterpret_cast<uint8_t*>( env->GetByteArrayElements(sBox, nullptr) );
     jsize cipherDataLength = env ->GetArrayLength(cipherData);
 
-    for (int i = 1; i < cipherDataLength + 1; i++) {
+    int i = 1;
+    for (;i + 16 <= cipherDataLength + 1; i += 16){
+        uint8x16_t keyStreamChunk = vld1q_u8(keyStreamBytes + ( i & 0xff));
+        uint8x16_t cipherDataBytesChunk = vld1q_u8(cipherDataBytes + i - 1);
+        uint8x16_t decryptedChunk = veorq_u8(cipherDataBytesChunk,keyStreamChunk);
+        vst1q_u8(cipherDataBytes + i - 1, decryptedChunk);
+    }
+
+    for (; i < cipherDataLength + 1; i++) {
         int j = i & 0xff;
         cipherDataBytes[i-1] ^= keyStreamBytes[j];
     }
