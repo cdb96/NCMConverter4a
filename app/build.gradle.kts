9ac7497f70ca5ad70b8d3090e5c185855f563a1d
cdb96 <kiilk1@163.com>
2025-07-14 17:25:01 +0800

    尝试支持github action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,10 +29,20 @@ android {
         proguardFiles()
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("SIGNING_STORE_FILE") ?: "app/release.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
